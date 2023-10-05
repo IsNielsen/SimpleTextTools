78c17c5e0c0753946832ca8b5155fc04d16a459b
Isaac Nielsen <a02363142@usu.edu>
2023-10-04 18:31:54 -0600

    First draft of wc, so far only the lines are being counted correctly

--- src/WordCount.py
+++ src/WordCount.py
@@ -20,4 +20,28 @@
 
 def wc(files):
     """print newline, word, and byte counts for each file"""
-    print("TODO: print newline, word, and byte counts for each file")
+    """
+        Take a list of filenames
+        for each file in the list:
+        open the file, count lines, words, and characters
+        print lines, words, characters, and filename
+    """
+    multFile = len(files) > 1  # true if more than 1 file
+    total_line, total_word, total_char = 0, 0, 0
+    for filename in files:
+        char_count, word_count, line_count = 0, 0, 0
+        file = open(filename)
+        for line in file:
+            if "" in line:  # every char
+                char_count += 1
+            if " " in line:  # check for words
+                word_count += 1
+            if "\n" in line:  # check for lines
+                line_count += 1
+        print(f"{line_count}\t{word_count}\t{char_count}\t{filename}")
+        total_line += line_count
+        total_word += word_count
+        total_char += char_count
+        file.close()
+    if multFile:
+        print(f"{total_line}\t{total_word}\t{total_char}\t TOTAL")
